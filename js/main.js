82168211d35ba79da3563c7be6fe987cfe9265d4
Mariah Laqua <mariahlaqua@gmail.com>
2022-12-18 15:36:13 +0100

    Updated commenting and removed code from main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
  * I am reworking this project in React.js. When it is complete,
  * I will link each project's respective README's to each other.
  * 
+ * You can uncomment the console.logs to see the API response,
+ * and the results of some of the manipulation here
  */
 window.onload = (event) => getFetch()
 document.querySelector('button').addEventListener('click', getFetch)
@@ -14,35 +16,39 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
+
+        //console.log(data)
         document.querySelector('h3').innerText = data.meals[0].strMeal
         document.querySelector('img').alt = `A thumbnail image of ${data.meals[0].strMeal}`
         document.getElementById('recipePic').src = data.meals[0].strMealThumb
         document.querySelector('p').innerText = data.meals[0].strInstructions
         document.getElementById('linkToAuthor').href = data.meals[0].strSource
         document.getElementById('linkToAuthor').style.visibility = "visible"
-        //h4.forEach((h4s) => h4s.style.visibility = 'visible')
-       
-       //object.entries returns an array for each key:value pair, forEach, check the value is not empty
-       // if true, push to ingredients array
+
+        
+// object.entries returns an array for each key:value pair, forEach, check the value is not empty
+// if true, push to ingredients array
+// filter ingredients array for actual values, rather than empty strings
+
         Object.entries(data.meals[0]).forEach(([key,value]) => key.includes('strIngredient') && value ? ingredients.push(value) : null)
-        //filter ingredients array for actual ingredients
-        //ingredients = ingredients.filter(word => word.includes('strIngredient'))
-        console.log(ingredients.length)
-        console.log(ingredients)
+
+        
+        //console.log(ingredients.length) uncomment to see results
+        //console.log(ingredients) uncomment to see results
         Object.entries(data.meals[0]).forEach(([key,value]) => key.includes('strMeasure') && value ? measurements.push(value) : null)
         
-        //measurements = measurements.splice(ingredients.length)
-        console.log(measurements)
+        //console.log(measurements)
         let list = document.querySelector('ul')
+        
         // ingredients.length is how many ingredients there are
+        //create a new list item
         ingredients.map(ingredient => {
             let newIngredient = document.createElement('li');
             newIngredient.innerText = `${measurements[ingredients.indexOf(ingredient)]}  ${ingredient}  `;
             list.append(newIngredient)
         })
         
-        //create a new list item
+       
     })  
       .catch(err => {
           console.log(`error ${err}`)
